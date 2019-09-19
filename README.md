8b13d041ea746f91be17209f8faf54f04e45a0a2
Job Ouddeken <jobouddeken@hotmail.com>
2019-09-19 08:40:10 +0200

    Update README and add PropTypes

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # React Router Zero Config
 
-Use the react-router-dom package with just a single element and a config.
+Use the react-router-dom package with just a single element and a config file in json.
+
+## Prerequisites
+
+1. Node
+2. NPM or Yarn
+
+## Installation
+
+Using NPM:
+
+```
+npm install @jouddeken/react-router-zero-config
+```
+Using Yarn
+```markdown
+yarn add @jouddeken/react-router-zero-config
+```
+
+## Usage
+
+1. Create a JSON config file with the following pattern:
+
+   ```javascript
+   import React from 'react'; // used for React.lazy
+   
+   export default [
+     {
+       id: 'home',
+       path: '/',
+       exact: true,
+       component: React.lazy(() => import('../path/to/component')),
+     },
+   ];
+   
+   ```
+
+2. Import the package and pass in the config as property to the Router component
+
+   ```javascript
+   import Router from '@jouddeken/react-router-zero-config';
+   import routeConfig from './path/to/config/file';
+   
+   const App = () => (
+     <Router config={routeConfig} />
+   );
+   
+   export default App;
+   
+   ```
+
+   
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React, { Suspense } from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const Router = ({ config, loadingComponent = 'Loading...' }) => {
+const Router = ({ config, loadingComponent }) => {
   return (
     <Suspense fallback={loadingComponent}>
       <BrowserRouter>
         <Switch>
-          {config.map(({ id, exact, path, component }) => (
+          {config.map(({
+            id,
+            path,
+            exact = false,
+            component
+          }) => (
             <Route
+              id={id}
               key={id}
-              exact={exact}
               path={path}
+              exact={exact}
               component={component}
             />
           ))}
@@ -19,5 +26,26 @@ const Router = ({ config, loadingComponent = 'Loading...' }) => {
     </Suspense>
   );
 };
+
+Router.propTypes = {
+  config: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      exact: PropTypes.bool,
+      component: PropTypes.node.isRequired,
+    }),
+  ).isRequired,
+  loadingComponent: PropTypes.oneOfType(
+    PropTypes.node,
+    PropTypes.string,
+    PropTypes.number,
+  ),
+};
+
+Router.defaultProps = {
+  config: [],
+  loadingComponent: 'Loading...'
+}
 
 export default Router;
